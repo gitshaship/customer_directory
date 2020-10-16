3d4cafc57a72b86c2fc0bd9f0e2975b973ae9ceb
shashiJ <sjayaweera92@gmail.com>
2020-10-16 16:55:00 +1100

    initial commit

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS piexchange;
-USE piexchange;
-CREATE TABLE IF NOT EXISTS customer (
-    customer_id int NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    contact_no varchar(255) NOT NULL,
-    PRIMARY KEY(customer_id)
-);
